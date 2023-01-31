06080c58001db09da884106d40e6f60df0db5e5b
euLita <litas.educ@gmail.com>
2023-01-31 17:21:10 -0400

    Create data base and module

--- api_activities/models.py
+++ api_activities/models.py
@@ -1,18 +1,31 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///activities.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False, bind=engine))
 
-base = declarative_base()   # default do sqlAlchemy
-base.query = db_session.query_property()
+Base = declarative_base()   # default do sqlAlchemy
+Base.query = db_session.query_property()
 
-class PatternTest(base):
-    __tablename__='Pattern'
+class PeoplesTest(Base):
+    __tablename__='peoples' # Nome da tabela no banco de dados
     id = Column(Integer, primary_key=True)
-    name = Column(String(100), index=True) # A fim de tornar a consulta mais rapida quando a mesma for pelo nome
+    name = Column(String(100), index=True) #A fim de tornar a consulta mais rapida quando a mesma for pelo nome
     age = Column(Integer)
 
-    def __repr__(self):
-        return '<Pattern {}>'.format(self.name)
+    def __repr__(self): #Representação do objeto
+        return '<People {}>'.format(self.name)
+
+class ActivitiesTest(Base):
+        __tablename__='activities'
+        id = Column(Integer, primary_key=True)
+        name = Column(String(200))
+        people_id = Column(Integer, ForeignKey('peoples.id'))   #PeoplesTest
+        people = relationship("PeoplesTest")
+
+def init_db():
+    Base.metadata.create_all(bind=engine) # create_all ele que vai criar o nosso banco de dados
+
+if __name__ == '__main__':
+    init_db()
